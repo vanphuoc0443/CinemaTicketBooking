a244e6a68dec688e48b4d208d5efb06bbd3f6b24
vanphuoc0443 <vanphuoc0443@gmail.com>
2026-01-15 23:45:30 +0700

    feat: configure database connection

---
+++ src/main/java/util/DatabaseConnection.java
@@ -0,0 +1,45 @@
+package util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(
+                "jdbc:mysql://localhost:3306/cinema_booking"
+                        + "?useSSL=false"
+                        + "&serverTimezone=UTC"
+                        + "&allowPublicKeyRetrieval=true"
+        );
+
+        config.setUsername("root");
+        config.setPassword("th0m4sj4m3s"); //Nhap mat khau server
+
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+
+        config.setAutoCommit(false);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    private DatabaseConnection() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
